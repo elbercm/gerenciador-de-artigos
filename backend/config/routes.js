2893e23c15cb4f7715554ce8d29e2bbd909cc241
Elber costa <elber.cm@hotmail.com>
2022-02-11 19:15:00 -0300

    Configurado o componente Home

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -55,8 +55,8 @@ module.exports = (app) => {
     .all(app.config.passport.authenticate())
     .get(app.api.article.getByCategory);
 
-  // app
-  //   .route("/stats")
-  //   .all(app.config.passport.authenticate())
-  //   .get(app.api.stat.get);
+  app
+    .route("/stats")
+    .all(app.config.passport.authenticate())
+    .get(app.api.stat.get);
 };
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,9 @@ import router from "./config/router";
 
 Vue.config.productionTip = false;
 
+// TEMPORARIO!
+require("axios").defaults.headers.common["Authorization"] =
+  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6Ik1hcmlhIFNpbHZhIiwiZW1haWwiOiJtYXJpYS5zaWx2YUBob3RtYWlsLmNvbSIsImFkbWluIjoiZmFsc2UiLCJpYXQiOjE2NDQ2MTU5NDQsImV4cCI6MTY0NDg3NTE0NH0.4y-Bt18EFbUKG9MgvFWIE1I82Lw9Wxs_I7DkEgn_X1o";
 new Vue({
   store,
   router,
