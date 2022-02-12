4ea8dc64fe31912292c7215170895940f3022bbb
Elber costa <elber.cm@hotmail.com>
2022-02-11 22:04:46 -0300

    Criado o cadastro de usuário

--- frontend/src/config/msgs.js
+++ frontend/src/config/msgs.js
@@ -6,14 +6,13 @@ Vue.use(Toasted, {
   duration: 3000,
 });
 
-Vue.Toasted.register(
+Vue.toasted.register(
   "defaultSuccess",
   (payload) => (!payload.msg ? "Operação realizada com sucesso!" : payload.msg),
-
   { type: "success", icon: "check" }
 );
 
-Vue.Toasted.register(
+Vue.toasted.register(
   "defaultError",
   (payload) => (!payload.msg ? "Oops.. Erro inesperado." : payload.msg),
   { type: "error", icon: "times" }
